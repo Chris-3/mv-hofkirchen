33517e18e1de740eb58a8aaed2e9572c5f42f5fe
chris-3 <choeftberger@gmail.com>
2022-09-10 10:13:43 +0200

    updated project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/voting-app'),
+      dir: require('path').join(__dirname, './coverage/mv-hofkirchen'),
       subdir: '.',
       reporters: [
         { type: 'html' },
